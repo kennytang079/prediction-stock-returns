ca1466bfebf7fdd5f9c23baa597a14d582fe6a4b
Kenny Tang <kennytang@Kennys-MacBook-Pro-7.local>
2026-07-26 22:36:35 -0700

    Creating a regression model to predict future stock behavior

--- main.py
+++ main.py
@@ -4,80 +4,80 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
 
-# #Downloaded the data
-# apple = yf.download("AAPL", start="2020-01-01", end="2025-01-01")
-# apple["Return"]= apple["Close"].pct_change()
+#Downloaded the data
+apple = yf.download("AAPL", start="2020-01-01", end="2025-01-01")
+apple["Return"]= apple["Close"].pct_change()
 
-# print(apple.head())
+print(apple.head())
 
-# #Plot of Stock Prices
-# apple["Close"].plot(figsize=(12,6))
-# plt.title("Apple Stock Price")
-# plt.xlabel("Date")
-# plt.ylabel("Price")
-# plt.show()
+#Plot of Stock Prices
+apple["Close"].plot(figsize=(12,6))
+plt.title("Apple Stock Price")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
 
-# #Plot of Stock Returns
-# apple["Return"].plot(figsize=(12,6))
-# plt.title("Apple Daily Return")
-# plt.xlabel("Date")
-# plt.ylabel("Daily Return")
-# plt.show()
+#Plot of Stock Returns
+apple["Return"].plot(figsize=(12,6))
+plt.title("Apple Daily Return")
+plt.xlabel("Date")
+plt.ylabel("Daily Return")
+plt.show()
 
-# #Statistics
-# print(apple["Return"].mean())
-# print(apple["Return"].std())
+#Statistics
+print(apple["Return"].mean())
+print(apple["Return"].std())
 
-# apple["Return"].hist(bins=50)
-# plt.title("Distribution of Apple Return")
-# plt.xlabel("Daily Returns")
-# plt.ylabel("Frequency")
-# plt.show()
+apple["Return"].hist(bins=50)
+plt.title("Distribution of Apple Return")
+plt.xlabel("Daily Returns")
+plt.ylabel("Frequency")
+plt.show()
 
 
-# #Rolling Statistics
-# apple["Rolling Volatility"] = apple["Return"].rolling(window=30).std()
-# apple["Rolling Volatility"].plot(figsize=(12,6))
-# plt.title("Apple 30 Day Rolling Volatility")
-# plt.xlabel("Date")
-# plt.ylabel("Volatility")
-# plt.show()
+#Rolling Statistics
+apple["Rolling Volatility"] = apple["Return"].rolling(window=30).std()
+apple["Rolling Volatility"].plot(figsize=(12,6))
+plt.title("Apple 30 Day Rolling Volatility")
+plt.xlabel("Date")
+plt.ylabel("Volatility")
+plt.show()
 
-# apple["MA30"] = apple["Close"].rolling(window=30).mean()
-# apple["MA100"] = apple["Close"].rolling(window=100).mean()
-# apple["Close"].plot(figsize=(12,6), label="Close Price")
-# apple["MA30"].plot(label="30 day MA")
-# apple["MA100"].plot(label="100 day MA")
+apple["MA30"] = apple["Close"].rolling(window=30).mean()
+apple["MA100"] = apple["Close"].rolling(window=100).mean()
+apple["Close"].plot(figsize=(12,6), label="Close Price")
+apple["MA30"].plot(label="30 day MA")
+apple["MA100"].plot(label="100 day MA")
 
-# plt.title("Apple Price with Moving Averages")
-# plt.xlabel("Date")
-# plt.ylabel("Price")
-# plt.legend()
-# plt.show()
+plt.title("Apple Price with Moving Averages")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.legend()
+plt.show()
 
-# #Prediction Statistics
-# apple["Signal"] = (apple["MA30"] > apple["MA100"]).astype(int)
-# apple["Signal"].plot(figsize=(12,4))
+#Prediction Statistics
+apple["Signal"] = (apple["MA30"] > apple["MA100"]).astype(int)
+apple["Signal"].plot(figsize=(12,4))
 
-# plt.title("Trend Signal")
-# plt.xlabel("Date")
-# plt.ylabel("Signal")
-# plt.show()
+plt.title("Trend Signal")
+plt.xlabel("Date")
+plt.ylabel("Signal")
+plt.show()
 
-# apple["Future Return"] = apple["Return"].shift(-1)
-# bullish_return = apple[apple["Signal"]==1]["Future Return"]
-# bearish_return = apple[apple["Signal"]==0]["Future Return"]
-# print(bullish_return.mean())
-# print(bearish_return.mean())
+apple["Future Return"] = apple["Return"].shift(-1)
+bullish_return = apple[apple["Signal"]==1]["Future Return"]
+bearish_return = apple[apple["Signal"]==0]["Future Return"]
+print(bullish_return.mean())
+print(bearish_return.mean())
 
-# #Hypothesis Testing
-# t_stat, p_value = ttest_ind(
-#     bullish_return.dropna(),
-#     bearish_return.dropna(),
-#     equal_var=False,
-# )
-# print("T Statistic", t_stat)
-# print("P Value", p_value)
+#Hypothesis Testing
+t_stat, p_value = ttest_ind(
+    bullish_return.dropna(),
+    bearish_return.dropna(),
+    equal_var=False,
+)
+print("T Statistic", t_stat)
+print("P Value", p_value)
 
 
 tickers = [
